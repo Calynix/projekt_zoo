8dd62a0b53c1354aa97e201a65adff570a6ed3dc
Calynix <undead.cat.8@gmail.com>
2025-01-12 04:34:45 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import AnimalProfile from './pages/AnimalProfile';
-import TicketPrices from './pages/TicketPrices';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TicketPage from "./pages/TicketPrices";
+import AnimalProfilePage from "./pages/AnimalProfiles";
+import NewsPage from "./pages/NewsPage";
+import Header from "./components/Header"; 
+const App = () => {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/animal-profiles" component={AnimalProfile} />
-          <Route path="/ticket-prices" component={TicketPrices} />
-        </Switch>
-        <Footer />
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<h2>Welcome to the Zoo!</h2>} />
+        <Route path="/tickets" element={<TicketPage />} />
+        <Route path="/animals" element={<AnimalProfilePage />} />
+        <Route path="/news" element={<NewsPage />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
