02935e6c4298ed04b7c25e773b77d366fb664690
Calynix <undead.cat.8@gmail.com>
2025-01-12 05:05:27 +0100

    update homepage
    - update homepage
    - add homepage to app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TicketPage from "./pages/TicketPrices";
 import AnimalProfilePage from "./pages/AnimalProfiles";
+import HomePage from "./pages/HomePage"; 
 import Header from "./components/Header"; 
+
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<h2>Welcome to the Zoo!</h2>} />
+        <Route path="/" element={<HomePage />} /> 
         <Route path="/tickets" element={<TicketPage />} />
         <Route path="/animals" element={<AnimalProfilePage />} />
       </Routes>
